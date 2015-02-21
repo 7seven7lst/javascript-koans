431cd08de3adcbf1290e0813f8088a5cb1a832a2
Seven Li <sevenlist0110@gmail.com>
2015-02-20 18:37:58 -0800

    Correct test case 3-4 and add function to sum up for AboutApplyingWhatWeHaveLearnt.js

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -64,14 +64,16 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
-
-    expect(233168).toBe(FILL_ME_IN);
+    var sum = _.range(1,1000).reduce(function(sum,x){
+      if (x%3 ===0 || x%5 === 0) return sum+x;
+      else return sum;
+    }, 0);    /* try chaining range() and reduce() */
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
